af4847ffb78971ba7ebfdc9378e3733a9fa2b028
Bairong Huang <brongh90@gmail.com>
2022-03-05 01:35:12 +0800

    backend set up

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # vaccination-is-good
+
+The system should support following features:
+● Support multiple vaccination centres in Singapore
+● Keep into account the daily supply of the nurses for each centre and their duty schedule
+● Model the daily capacity of a centre and accordingly allocate the vaccination slots to
+the residents
+● Support CRUD operations for the slot reservation
+● Avoid double reservation (What happens if the same user books again?)
+● The solution should handle the resource contention (What happens if 2 different users
+book the same time slot?)
---
+++ api/index.ts
@@ -0,0 +1,28 @@
+import bodyParser from "body-parser";
+import cors from "cors";
+import express from "express";
+import helmet from "helmet";
+import mongoose from "mongoose";
+
+import router from "./controllers";
+
+const startServer = async () => {
+  const app = express();
+
+  app.use(bodyParser.json());
+  app.use(bodyParser.urlencoded({ extended: true }));
+  app.use(cors());
+  app.use(helmet());
+
+  await mongoose.connect("mongodb://localhost:27017/vaccine");
+
+  app.use(router);
+
+  app.listen(8000, () => {
+    console.log("=====================================");
+    console.log("Server is now listening on port: 8000");
+    console.log("=====================================");
+  });
+};
+
+startServer();
